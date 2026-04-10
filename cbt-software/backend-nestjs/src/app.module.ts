d1d98f13623b4af88b7122dc929a0cc87dce8c3b
youngemeritus <sobalajetomiwa@gmail.com>
2026-04-10 04:53:47 -0700

    feat: implement jwt auth modules

--- cbt-software/backend-nestjs/src/app.module.ts
+++ cbt-software/backend-nestjs/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
     UsersModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
---
+++ cbt-software/backend-nestjs/src/modules/auth/auth.service.ts
@@ -0,0 +1,21 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from '../users/users.service';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async login(email: string, pass: string) {
+    const user = await this.usersService.findByEmail(email);
+    if (user && await bcrypt.compare(pass, user.password)) {
+      const payload = { sub: user.id, email: user.email, role: user.role };
+      return { access_token: await this.jwtService.signAsync(payload) };
+    }
+    throw new UnauthorizedException();
+  }
+}
